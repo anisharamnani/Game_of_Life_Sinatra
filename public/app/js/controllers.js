5f2c3e0467250e98b3b36c5b894336229aca7a83
anisharamnani <anisha.ramnani@gmail.com>
2014-02-12 12:12:08 -0500

    display one particular board

--- public/app/js/controllers.js
+++ public/app/js/controllers.js
@@ -5,6 +5,6 @@ var gameoflifeControllers = angular.module('gameoflifeControllers', []);
 gameoflifeApp.controller('RandBoardCtrl',['$scope', '$http',
   function($scope, $http){
     $http.get('http://localhost:9393/random_board').success(function(data){
-      $scope.board = data;
+      $scope.boards = data;
     });
   }]);
